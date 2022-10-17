779abb3f994877c037109757ab851793ea60a427
Rakib <hasan.rakib@datasoft-bd.com>
2022-10-17 12:03:13 +0600

    BLANK PROJECT PUSH!

---
+++ app/src/main/java/rakib/hasan/inappupdatesconfig/MainActivity.kt
@@ -0,0 +1,11 @@
+package rakib.hasan.inappupdatesconfig
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
